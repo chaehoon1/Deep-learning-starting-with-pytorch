0b9919daa4c5e5ac17d2b51f88b6cbedf1a0b056
chaehoon <yurim7p@gmail.com>
2024-05-18 21:59:02 +0900

    commit

---
+++ neuralNetwork.py
@@ -0,0 +1,8 @@
+x, y =get_data()
+w, b = get_weights()
+for i in range(50000) :
+    y_pred = simple_network(x)
+    loss = loss_fn(y, y_pred)
+    if i % 5000 == 0:
+        print(loss)
+    optimize(learning_rate)
