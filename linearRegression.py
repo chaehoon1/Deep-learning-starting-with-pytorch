58406769b86f4241ca5f1b646caa673fbc5f36a9
chaehoon <yurim7p@gmail.com>
2024-05-25 22:18:04 +0900

    commit

--- linearRegression.py
+++ linearRegression.py
@@ -2,3 +2,8 @@ import torch
 
 train_x = torch.FloatTensor([1, 2, 3, 4, 5])
 train_y = torch.FloatTensor([3, 5, 7, 9, 11])
+
+param_w = torch.FloatTensor([3])
+param_b = torch.FloatTensor([2])
+
+prediction = param_w * train_x + param_b
