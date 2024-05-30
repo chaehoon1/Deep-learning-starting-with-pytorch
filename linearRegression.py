f3789812d7e8643c8b3dd0b921b5e65bbd6a62fe
chaehoon <yurim7p@gmail.com>
2024-05-31 03:47:25 +0900

    commit

--- linearRegression.py
+++ linearRegression.py
@@ -3,19 +3,36 @@ import torch
 train_x = torch.FloatTensor([1, 2, 3, 4, 5])
 train_y = torch.FloatTensor([3, 5, 7, 9, 11])
 
-param_w = torch.FloatTensor([3])
-param_b = torch.FloatTensor([2])
+param_w = torch.tensor([3], dtype = torch.float, requires_grad = True)
+param_b = torch.tensor([2], dtype = torch.float, requires_grad = True)
 
-prediction = param_w * train_x + param_b  #y=wx+b
+prediction = train_x * param_w + param_b
+print(prediction)
 
-loss = torch.mean((prediction - train_y) ** 2)
+loss_fn = torch.mean((prediction - train_y) ** 2)
+loss_fn.backward()
+print(param_w.grad) # x = 2, y = 5인 경우 : loss = (2w + b - 5)^2에서 loss를 w에 대해 편미분 후 w = 3, b = 2 대입...모든 (x, y)에 대해 수행 후 평균
+print(param_b.grad) # x = 2, y = 5인 경우 : loss = (2w + b - 5)^2에서 loss를 b에 대해 편미분 후 w = 3, b = 2 대입...모든 (x, y)에 대해 수행 후 평균
+param_w.grad.data.zero_()
+param_b.grad.data.zero_()
 
-model_w = torch.tensor([3], dtype = torch.float, requires_grad = True)
-model_b = torch.tensor([2], dtype = torch.float, requires_grad = True)
+def optimizer(train_x, train_y, param_w, param_b, learning_rate, epoch) :
+    for i in  range(epoch) :    
+        prediction = param_w * train_x + param_b
+        loss_fn = torch.mean((prediction - train_y) ** 2)
+        
+        loss_fn.backward()
 
-prediction = model_w * train_x + model_b
+        param_w.data -= learning_rate * param_w.grad
+        param_b.data -= learning_rate * param_b.grad
+       
+        if (i + 1) % 10 == 0 :    
+            print(f'Epoch [{i + 1}/100], Loss: {loss_fn.item():.4f}')
+            print('prediction : ', prediction)
+            print('param_w : ', param_w)
+            print('param_b : ', param_b)
 
-loss = torch.mean((prediction - train_y) ** 2)
+        param_w.grad.data.zero_()
+        param_b.grad.data.zero_()
 
-def gd(w ,b ,learning_data, epoch) :
-
+optimizer(train_x, train_y, param_w, param_b, 0.01, 1000)
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 import torch
+
 x = torch.randn(3,5)
 print(x)
+
+
+y = torch.rand(1)
+print(y)
+
+z = torch.rand(3)
+print(z)
+
+w = torch.rand(2, 2, 2)
+print(w)
